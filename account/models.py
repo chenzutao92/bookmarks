f3661a2e34f05b86a2ac6b76380c29301ff41de6
Your Name <email@example.com>
2017-12-06 21:29:04 +0800

    第六章开始，为用户添加视图

--- account/models.py
+++ account/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
@@ -9,3 +10,23 @@ class Profile(models.Model):
 
     def __str__(self):
         return 'Profile for user {}'.format(self.user.username)
+
+
+class Contact(models.Model):
+    user_from = models.ForeignKey(User, related_name='rel_from_set')
+    user_to = models.ForeignKey(User, related_name='rel_to_set')
+    # db_index=True来创建一个数据库索引给created字段。这会提升查询执行的效率当通过这个字段对查询集（QuerySets）进行排序的时候。
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return '{}follows {}'.format(self.user_from,self.user_to)
+
+# 动态添加以下字段给用户
+User.add_to_class('following', models.ManyToManyField('self',through=Contact,
+                                                      related_name='followers',
+                                                      symmetrical=False))
+# symmetrical=Flase来定义一个非对称（non-symmetric）关系。这表示如果我关注了你，你不会自动的关注我。
+
